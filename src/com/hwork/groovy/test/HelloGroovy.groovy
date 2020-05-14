d7e034d5dfa8700e5b1923a37b8e751bb3ef8396
杨胜举 <yangshengju@jsh.com>
2020-05-15 05:51:00 +0800

    first groovy

---
+++ src/com/hwork/groovy/test/HelloGroovy.groovy
@@ -0,0 +1,2 @@
+package com.hwork.groovy.test
+println "hello groovy"
---
+++ src/com/hwork/groovy/variable/variablestudy.groovy
@@ -0,0 +1,16 @@
+package com.hwork.groovy.variable
+
+int x =10
+println x.class
+
+double y=3.14
+println(y.class)
+
+def x1=11
+def y1=3.1415
+def name="ysj"
+
+println([x1.class,y1.class,name.class])
+
+x1='asdfdsf'
+println(x1.class)
