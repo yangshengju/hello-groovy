68ab8ffdd82c4c50c0695b399711b1ba30299d3e
杨胜举 <yangshengju@jsh.com>
2020-05-15 07:46:21 +0800

    groovy string

---
+++ src/com/hwork/groovy/variable/stringtest.groovy
@@ -0,0 +1,11 @@
+package com.hwork.groovy.variable
+
+def name='line one' +
+        'line 2' +
+        'line 3'
+println name
+
+def tripleName='''triple string line one
+line2
+line3'''
+println tripleName
