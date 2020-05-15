b908eec270774cac20150b418af89f2a158610fb
杨胜举 <yangshengju@jsh.com>
2020-05-16 05:58:17 +0800

    可扩展字符串GString测试

--- src/com/hwork/groovy/variable/stringtest.groovy
+++ src/com/hwork/groovy/variable/stringtest.groovy
@@ -3,9 +3,32 @@ package com.hwork.groovy.variable
 def name='line one' +
         'line 2' +
         'line 3'
-println name
+//println name
 
-def tripleName='''triple string line one
-line2
-line3'''
+def tripleName='''\
+{
+  "sub": "1234567890",
+  "name": "John Doe",
+  "iat": 1516239022
+}'''
 println tripleName
+
+//可扩展字符串
+def doubleName = "This is a common string";
+println doubleName.class
+
+def person="ysj"
+def sayHello = "Hello:${person}"
+println(sayHello)
+println(sayHello.class)
+
+def sum = "The sum of 2 and 3 is ${2+3}"
+println(sum)
+println("sum.class："+sum.class)
+
+String echo(String message) {
+    return message
+}
+
+def echoStr = echo(sum);
+println("echoStr : "+echoStr)
